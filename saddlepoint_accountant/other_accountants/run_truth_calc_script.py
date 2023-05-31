96cc186597617c13915398466532c5825d91d1e7
Felipe-Gomez <74742200+Felipe-Gomez@users.noreply.github.com>
2023-05-31 01:45:03 -0400

    Update run_truth_calc_script.py

--- saddlepoint_accountant/other_accountants/run_truth_calc_script.py
+++ saddlepoint_accountant/other_accountants/run_truth_calc_script.py
@@ -5,18 +5,18 @@ import time
 from .truth_accountant import *
 from multiprocessing import Pool
 
-'''
-RUNTIME OF CODE ON 64-CORE CLUSTER: 
-'''
+#set the accuracy of the privacy parameters to 20 digits
 mp.mp.dps = 20
 
+#initialize parameters
 sigma = 2; q = mp.mpf('0.01'); delta = mp.mpf('1e-15')
 comps_lst = np.linspace(1.5,4.5,32)*1000
 
+#grab clt bound estimates 
 with open("eps_clt_bounds_n1500_4500_32.pkl", "rb") as fb:
     clt_bounds = pickle.load(fb)
     clt_bounds = np.array(clt_bounds)[0,:,:]
-    
+
 def temp_compute_epsilon(comp):
 
     index = np.where( np.isclose(comps_lst, comp))[0][0]
@@ -35,6 +35,6 @@ if __name__ == "__main__":
 
     print(f"Wall time for 32 evaluations: {t2-t1} seconds")
 
-    with open("mpmath_s2_q0.01_dm15_n1500_4500_32_.pkl", "wb") as fb:
+    with open("mpmath_s2_q0.01_delta_1em15_n1500_4500_32.pkl", "wb") as fb:
         pickle.dump(eps_mpmath, fb)
         
