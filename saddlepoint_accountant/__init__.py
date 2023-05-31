f47900e2b9ea2cb6b1f8125b86f37c248635861d
Felipe-Gomez <74742200+Felipe-Gomez@users.noreply.github.com>
2023-05-31 00:33:39 -0400

    Update __init__.py

--- saddlepoint_accountant/__init__.py
+++ saddlepoint_accountant/__init__.py
@@ -1,3 +1,4 @@
 from saddlepoint_accountant.abstract_privacy_mechanism import PrivacyMechanism
 from saddlepoint_accountant.gaussian_mechanism import GaussianMechanism
 from saddlepoint_accountant.accountant import SaddlePointAccountant
+from saddlepoint_accountant import other_accountants
