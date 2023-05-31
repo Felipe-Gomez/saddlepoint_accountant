69da4f6be2d704e571e8a2b392699476254ba56d
Felipe-Gomez <74742200+Felipe-Gomez@users.noreply.github.com>
2023-05-30 23:18:11 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+version = '0.1'
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='SaddlePoint',
+    version=version,
+    description='The saddlepoint accountant for approximating DP guarantees for the DP-SGD algorithm under various DP mechanisms', 
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Felipe-Gomez/SaddlePoint',
+    author='Felipe Gomez',
+    packages=["SaddlePoint"],
+    python_requires=">=3.7.0",
+    include_package_data=True,
+    setup_requires=["numpy", "scipy", "sympy", "mpmath"],
+    extras_require={
+        "extra": [
+            "jupyter",
+        ]
+    },
+    install_requires=[
+        "prv_accountant",
+        "scipy",
+        "numpy",
+        "mpmath",
+        "sympy"
+    ],
+    zip_safe=False
+)
